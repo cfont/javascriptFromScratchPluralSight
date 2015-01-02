a86a154168ee929ab38c2ecff04e347c861b90eb
Chris Fontenot <chris.fontenot@ellucian.com>
2015-01-02 17:35:12 -0600

    added new statement and included braces

--- m3/js/script.js
+++ m3/js/script.js
@@ -1,7 +1,8 @@
 var ageAsString = prompt("What is your age?", "");
 var age = Number(ageAsString);
-if ( age < 40 )
+if ( age < 40 ) {
   alert("Oh you're so young...");
-
+  alert("and 40 is a drag...");
+}
 
 alert("Thank you");
