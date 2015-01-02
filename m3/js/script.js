889ed2c95db30d8bacfea97e6246f1f3e3a8586d
Chris Fontenot <chris.fontenot@ellucian.com>
2015-01-02 17:48:42 -0600

    combine lines of code
    clean it up

--- m3/js/script.js
+++ m3/js/script.js
@@ -1,14 +1,1 @@
-var ageAsString = prompt("What is your age?", "");
-var age = Number(ageAsString);
-if ( age < 40 ) {
-  alert("Oh you're so young...");
-  alert("and 40 is a drag...");
-}
-else if (age == 40) {
-  alert("Hey, you're 40!");
-}
-else {
-  alert("Don't worry, you're young at heart");
-}
-
-alert("Thank you");
+var age = Number(prompt("What is your age?", ""));
