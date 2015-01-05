0b8f7ff930ee8a64b8aad268d431e3277e0429b4
Chris Fontenot <chris.fontenot@ellucian.com>
2015-01-05 10:32:57 -0600

    testing not/! operator

--- m4/js/script.js
+++ m4/js/script.js
@@ -4,6 +4,9 @@ var isOld= true;
 
 // var isOldMan = isMale && isOld;
 
-var isOldOrFemale = isFemale || isOld;
+//var isOldOrFemale = isFemale || isOld;
 
-alert(isOldOrFemale);
+//alert(isOldOrFemale);
+
+var isMale = ! isFemale;
+alert(isMale);
