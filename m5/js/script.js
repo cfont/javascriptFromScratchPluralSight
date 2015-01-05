0f3796515b7075a808010d7fa817e88398df675d
Chris Fontenot <chris.fontenot@ellucian.com>
2015-01-05 17:05:36 -0600

    introducing recursion concept

--- m5/js/script.js
+++ m5/js/script.js
@@ -1,12 +1,30 @@
-var scope = "global";
-// closure means that the something happens in the scope that it is declared not in the scope that it is invoke. a function will scope its variables at the time it is declared, not at the time it is run. 
-
-function  testScope(){
-  var scope = "local";
-  function innerFunc() {return scope;}
-  return innerFunc;
+function func1(num,exp){
+  if (exp === 0){
+    return 1;
+  }
+  return num * func2(num,exp-1);
 }
 
-var innerFunc = testScope();
-var answer = innerFunc();
+function func2(num,exp){
+  if (exp === 0){
+    return 1;
+  }
+  return num * func3(num,exp-1);
+}
+
+function func3(num,exp){
+  if (exp === 0){
+    return 1;
+  }
+  return num * func4(num,exp-1);
+}
+
+function func4(num,exp){
+  if (exp === 0){
+    return 1;
+  }
+  alert("exponent too big!");
+}
+
+var answer = func1(4,3);
 output.innerHTML = answer;
