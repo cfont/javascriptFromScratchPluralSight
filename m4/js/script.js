23cb7b8f551debd0c59d7bc902dedf32c5e3b871
Chris Fontenot <chris.fontenot@ellucian.com>
2015-01-05 10:46:14 -0600

    testing the identity operator

--- m4/js/script.js
+++ m4/js/script.js
@@ -1,3 +1,7 @@
 // the equality (==) operator uses type conversion to decide if these things are equal. Reference --> http://www.c-point.com/javascript_tutorial/jsgrpComparison.htm
 
-alert(0==false);
+// alert(0==false); // returns true
+
+// the identity (===) operator behaves the same way except that it does not do any type conversion. So the two things being compared must be the same type in order to be equal.
+
+alert (0===false); //returns false
