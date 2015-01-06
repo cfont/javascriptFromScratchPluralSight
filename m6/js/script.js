56041c4b525ae6744eb27cfa4ec212b8bd00478b
Chris Fontenot <chris.fontenot@ellucian.com>
2015-01-05 21:21:45 -0600

    slightly more complicated regex

--- m6/js/script.js
+++ m6/js/script.js
@@ -6,7 +6,10 @@ var concat = string1 + string2
 // for regex search it must go between forward slashs
 // can use literals like "quick"
 // can use [a-z]* meaning any lowercase characters and any number of those characters
+// {5} means i'm looking for 5 of those characters
+// \b means i want that search in a word boundry
 
-var offset = concat.search(/[a-z]*/);
+
+var offset = concat.search(/\b[a-z]{5}\b/);
 
 output.innerHTML = offset;
