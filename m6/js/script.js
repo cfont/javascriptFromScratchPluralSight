fcd27a03f44dfd9f135c506ab12ed3ddffeff35f
Chris Fontenot <chris.fontenot@ellucian.com>
2015-01-05 20:53:07 -0600

    priming the array with values and retrieving one

--- m6/js/script.js
+++ m6/js/script.js
@@ -4,4 +4,9 @@ var empty = [];
 empty[0] = 1;
 empty[1] = 2;
 
-output.innerHTML = empty;
+//output.innerHTML = empty;
+
+var primes = [1,2,3,5,7,11];
+var thirdPrime = primes[2];
+
+//output.innerHTML = thirdPrime;
