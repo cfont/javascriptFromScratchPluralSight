42812e19a4872d5b7295007358b0e9608827a529
Chris Fontenot <chris.fontenot@ellucian.com>
2015-01-05 21:15:34 -0600

    using split and slice for strings

--- m6/js/script.js
+++ m6/js/script.js
@@ -1,6 +1,12 @@
-var string1 = "The quick 'brown' fox.";
-var string2 = 'The quick "brown" fox.';
+var string1 = "The quick brown fox";
+var string2 = " and lived happily ever after.";
 
+var concat = string1 + string2
 
+var words = concat.split(" ");
 
-output.innerHTML = string2;
+var indexOfBrown = concat.indexOf("brown");
+var indexOfAnd = concat.indexOf("and");
+var myFox = concat.slice(indexOfBrown,indexOfAnd);
+
+output.innerHTML = myFox;
